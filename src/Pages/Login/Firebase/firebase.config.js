50252531d36fd83743138e7afd4e2ac8cd373e8c
nmso2 <nmsubho@gmail.com>
2021-11-26 09:52:19 +0600

    updatr

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,19 +1,11 @@
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyD9JU34e1oyOJAKNF8yBLs9pKUgtvJ9FAs",
-    authDomain: "sunglass-collection.firebaseapp.com",
-    projectId: "sunglass-collection",
-    storageBucket: "sunglass-collection.appspot.com",
-    messagingSenderId: "321908559248",
-    appId: "1:321908559248:web:a23b85016d782dd41bf715"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
+
 
 export default firebaseConfig;
